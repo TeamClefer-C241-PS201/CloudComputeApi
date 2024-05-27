ae9e59c9bef101dfaf64a7d4e3f8f3f99e4c6ed6
syfmumtaz <syifamumtaz.22@gmail.com>
2024-05-27 18:05:55 +0700

    feat: migration

---
+++ app/database/migrations/20240527_create_tables.js
@@ -0,0 +1,53 @@
+const knex = require('knex');
+
+exports.up = async (knex) => {
+  await knex.schema.createTable('users', (table) => {
+    table.increments('userId').primary();
+    table.string('googleId', 255);
+    table.string('firstName', 255).notNullable();
+    table.string('lastName', 255).notNullable();
+    table.string('userName', 255).notNullable();
+    table.string('email', 255).notNullable().unique();
+    table.string('password', 255).notNullable();
+    table.string('userPhoto', 255);
+  });
+
+  await knex.schema.createTable('comments', (table) => {
+    table.increments('commentId').primary();
+    table.string('postComment', 255).notNullable();
+  });
+
+  await knex.schema.createTable('posts', (table) => {
+    table.increments('postId').primary();
+    table.integer('userId').unsigned().notNullable();
+    table.foreign('userId').references('userId').inTable('users');
+    table.integer('commentId').unsigned();
+    table.foreign('commentId').references('commentId').inTable('comments'); // Might be null
+    table.string('postTitle', 255).notNullable();
+    table.string('postDesc', 255).notNullable();
+    table.string('postDate', 255).notNullable();
+    table.integer('postLike').defaultTo(0);
+  });
+  // Articles table
+  await knex.schema.createTable('articles', (table) => {
+    table.increments('articleId').primary();
+    table.string('articleTitle', 255).notNullable();
+    table.string('articleDesc', 255).notNullable();
+  });
+  // Diseases table
+  await knex.schema.createTable('diseases', (table) => {
+    table.increments('diseaseId').primary();
+    table.integer('articleId').unsigned().notNullable();
+    table.foreign('articleId').references('articleId').inTable('articles');
+    table.string('diseaseName', 255).notNullable();
+    table.string('recommendation', 255);
+  });
+};
+
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('diseases');
+  await knex.schema.dropTableIfExists('articles');
+  await knex.schema.dropTableIfExists('comments');
+  await knex.schema.dropTableIfExists('posts');
+  await knex.schema.dropTableIfExists('users');
+};
---
+++ knexfile.js
@@ -0,0 +1,20 @@
+module.exports = {
+    development: {
+      client: 'mysql',
+      connection: {
+        host: 'localhost',
+        port: 3306,
+        database: 'cleferdev', // Nama database Anda
+        user: 'root', // Username database Anda
+        password: '' // Password database Anda
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+        directory: '.app/database/migrations'
+      }
+    }
+  };
