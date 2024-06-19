2712b52caf7265311ec29643691bf8594aa4ef90
Rafiarian <rafiyusuf2@gmail.com>
2024-06-20 00:12:33 +0700

    fix: add name into get all post

--- app/models/post.js
+++ app/models/post.js
@@ -1,51 +1,56 @@
 // models/Post.js
-const db = require('../config/dbConfig');
+const db = require("../config/dbConfig");
 
 const Post = {
   create: async (userId, postTitle, postDesc) => {
     try {
       const postDate = new Date().toISOString();
-      const [result] = await db.execute('INSERT INTO posts (userId, postTitle, postDesc, postDate) VALUES (?, ?, ?, ?)', [userId, postTitle, postDesc, postDate]);
+      const [result] = await db.execute(
+        "INSERT INTO posts (userId, postTitle, postDesc, postDate) VALUES (?, ?, ?, ?)",
+        [userId, postTitle, postDesc, postDate]
+      );
       return result.insertId;
     } catch (error) {
       throw new Error(error.message);
     }
   },
 
-    getAll: async (userId) => {
-      try {
-        const [rows] = await db.execute(
-          `SELECT p.*, 
-                  COALESCE(l.likeCount, 0) AS likerCount, 
-                  COALESCE(ls.likeStatus, 0) AS likeStat, 
-                  COALESCE(c.commentCount, 0) AS commentCount 
-           FROM posts p 
-           LEFT JOIN (
-               SELECT postId, COUNT(*) AS likeCount 
-               FROM likepost 
-               GROUP BY postId
-           ) l ON p.postId = l.postId 
-           LEFT JOIN (
-               SELECT postId, 1 AS likeStatus 
-               FROM likepost 
-               WHERE userId = ?
-           ) ls ON p.postId = ls.postId 
-           LEFT JOIN (
-               SELECT postId, COUNT(*) AS commentCount 
-               FROM comments 
-               GROUP BY postId
-           ) c ON c.postId = p.postId`,
-          [userId || null]
-        );
-        return rows;
-      } catch (error) {
-        throw new Error(error.message);
-      }
-    },
+  getAll: async (userId) => {
+    try {
+      const [rows] = await db.execute(
+        `SELECT p.*, 
+       u.name,
+       COALESCE(l.likeCount, 0) AS likerCount, 
+       COALESCE(ls.likeStatus, 0) AS likeStat, 
+       COALESCE(c.commentCount, 0) AS commentCount 
+          FROM posts p 
+          LEFT JOIN users u ON p.userId = u.userId
+        LEFT JOIN (
+        SELECT postId, COUNT(*) AS likeCount 
+          FROM likepost 
+        GROUP BY postId
+        ) l ON p.postId = l.postId 
+      LEFT JOIN (
+        SELECT postId, 1 AS likeStatus 
+        FROM likepost 
+       WHERE userId = ?
+        ) ls ON p.postId = ls.postId 
+      LEFT JOIN (
+    SELECT postId, COUNT(*) AS commentCount 
+    FROM comments 
+    GROUP BY postId
+    ) c ON c.postId = p.postId`,
+        [userId || null]
+      );
+      return rows;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
 
-    getById: async (postId, userId) => {
-      try {
-        const query = `
+  getById: async (postId, userId) => {
+    try {
+      const query = `
           SELECT 
             p.userId, 
             p.postId, 
@@ -76,39 +81,43 @@ const Post = {
           ) ls ON p.postId = ls.postId
           WHERE p.postId = ?
         `;
-    
-        const [rows] = await db.execute(query, [userId||null , postId]);
-    
-        if (rows.length === 0) {
-          throw new Error('Post not found');
-        }
-        
-        return rows[0];
-      } catch (error) {
-        throw new Error(error.message);
+
+      const [rows] = await db.execute(query, [userId || null, postId]);
+
+      if (rows.length === 0) {
+        throw new Error("Post not found");
       }
-    },
+
+      return rows[0];
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  },
 
   deleteById: async (postId) => {
     try {
-      const [result] = await db.execute('DELETE FROM posts WHERE postId = ?', [postId]);
+      const [result] = await db.execute("DELETE FROM posts WHERE postId = ?", [
+        postId,
+      ]);
       if (result.affectedRows === 0) {
-        throw new Error('Post not found');
+        throw new Error("Post not found");
       }
-      return 'Post deleted successfully';
+      return "Post deleted successfully";
     } catch (error) {
       throw new Error(error.message);
     }
   },
   like: async (userId, postId) => {
     try {
-      const [like] = await db.execute("Select postLikeId FROM likepost where userId= ? AND postId = ?",[userId,postId]);
-      const firstlike = like[0]
-      if(like.length > 0){
-        await db.execute(
-          "DELETE FROM likepost WHERE postLikeId = ?",
-          [firstlike.postLikeId]
-        );
+      const [like] = await db.execute(
+        "Select postLikeId FROM likepost where userId= ? AND postId = ?",
+        [userId, postId]
+      );
+      const firstlike = like[0];
+      if (like.length > 0) {
+        await db.execute("DELETE FROM likepost WHERE postLikeId = ?", [
+          firstlike.postLikeId,
+        ]);
         return "Unlike Post!";
       }
       const [result] = await db.execute(
@@ -119,8 +128,7 @@ const Post = {
     } catch (error) {
       throw new Error(error.message);
     }
-  }
+  },
 };
-
 
 module.exports = Post;
